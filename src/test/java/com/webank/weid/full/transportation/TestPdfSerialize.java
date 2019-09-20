4a7833a4d56450e8fc0085c58fed4c3e3e096c6a
rockyxia(夏石龙) <rockyxia@webank.com>
2019-09-20 19:12:39 +0800

    pdf test case

--- src/test/java/com/webank/weid/full/transportation/TestPdfSerialize.java
+++ src/test/java/com/webank/weid/full/transportation/TestPdfSerialize.java
@@ -1,17 +1,23 @@
 package com.webank.weid.full.transportation;
 
 import java.io.OutputStream;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import mockit.Mock;
 import mockit.MockUp;
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.webank.weid.common.LogUtil;
 import com.webank.weid.constant.ErrorCode;
+import com.webank.weid.protocol.base.Credential;
+import com.webank.weid.protocol.base.CredentialPojo;
 import com.webank.weid.protocol.base.PresentationE;
 import com.webank.weid.protocol.response.ResponseData;
 import com.webank.weid.suite.api.transportation.TransportationFactory;
@@ -20,6 +26,7 @@ import com.webank.weid.suite.api.transportation.params.ProtocolProperty;
 import com.webank.weid.suite.crypto.CryptService;
 import com.webank.weid.suite.crypto.CryptServiceFactory;
 import com.webank.weid.suite.entity.CryptType;
+import com.webank.weid.util.DataToolUtils;
 
 /**
  * test serialize class.
@@ -27,6 +34,10 @@ import com.webank.weid.suite.entity.CryptType;
 public class TestPdfSerialize extends TestBaseTransportation {
 
     private static final Logger logger = LoggerFactory.getLogger(TestPdfSerialize.class);
+    private static CredentialPojo credentialPojo = null;
+    private static CredentialPojo credentialPojoNew = null;
+    private static volatile CredentialPojo selectiveCredentialPojo = null;
+    private static volatile Credential credential = null;
     private static PresentationE presentation;
     private static PresentationE presentation4MlCpt;
     private static PresentationE presentation4MultiCpt;
@@ -39,6 +50,19 @@ public class TestPdfSerialize extends TestBaseTransportation {
         super.testInit4MlCpt();
         super.testInit4MultiCpt();
         super.testInitSpecTplCpt();
+        if (credentialPojo == null) {
+            credentialPojo = super.createCredentialPojo(createCredentialPojoArgs);
+        }
+        if (credentialPojoNew == null) {
+            credentialPojoNew = super.createCredentialPojo(createCredentialPojoArgsNew);
+        }
+        if (selectiveCredentialPojo == null) {
+            selectiveCredentialPojo = this.createSelectiveCredentialPojo(credentialPojo);
+        }
+        if (credential == null) {
+            credential = super.createCredential(createCredentialArgs)
+                .getCredential();
+        }
 
         mockMysqlDriver();
         presentation = getPresentationE();
@@ -52,7 +76,7 @@ public class TestPdfSerialize extends TestBaseTransportation {
      * 单级CPT测试.
      */
     @Test
-    public void testSerializeCase1() {
+    public void testSerialize_signalCptPolicy() {
         ResponseData<OutputStream> response = TransportationFactory
             .newPdfTransportation()
             .serialize(
@@ -76,11 +100,323 @@ public class TestPdfSerialize extends TestBaseTransportation {
                 presentation4MlCpt,
                 new ProtocolProperty(EncodeType.ORIGINAL),
                 weIdAuthentication);
+        LogUtil.info(logger, "presentiation", presentation4MlCpt);
         LogUtil.info(logger, "serialize", response);
         Assert.assertEquals(ErrorCode.SUCCESS.getCode(), response.getErrorCode().intValue());
         Assert.assertNotNull(response.getResult());
     }
 
+    /**
+     * biggest length test.
+     */
+    @Test
+    public void testSerialize_biggestLength() {
+        PresentationE copyPresention = DataToolUtils.clone(presentation4MlCpt);
+        List<CredentialPojo> credentialPojoList = copyPresention.getVerifiableCredential();
+        CredentialPojo credentialPojo = credentialPojoList.size()==1
+            ? credentialPojoList.get(0):credentialPojoNew;
+        for (int i = 0; i < 5; i++) {
+            credentialPojoList.add(credentialPojo);
+        }
+        ResponseData<OutputStream> response = TransportationFactory
+            .newPdfTransportation()
+            .serialize(
+                copyPresention,
+                new ProtocolProperty(EncodeType.ORIGINAL),
+                weIdAuthentication);
+        LogUtil.info(logger, "serialize", response);
+        Assert.assertEquals(ErrorCode.SUCCESS.getCode(), response.getErrorCode().intValue());
+        Assert.assertNotNull(response.getResult());
+
+        //Deserialize
+        ResponseData<PresentationE> resDeserialize = TransportationFactory
+            .newPdfTransportation().deserialize(response.getResult(), PresentationE.class,
+                 weIdAuthentication);
+        Assert.assertEquals(ErrorCode.SUCCESS.getCode(), resDeserialize.getErrorCode().intValue());
+        Assert.assertEquals(copyPresention.toJson(), resDeserialize.getResult().toJson());
+    }
+
+    /**
+     * 修改 presentation，设置其中的credentialPojo为空，序列化成功，但是文件打不开.
+     */
+    @Ignore
+    @Test
+    public void testSerialize_credentialPojoIsNull() {
+        PresentationE copyPresentationE = DataToolUtils.clone(presentation);
+        List<CredentialPojo> credentialPojoList =  copyPresentationE.getVerifiableCredential();
+        credentialPojoList.remove(0);
+        ResponseData<OutputStream> response = TransportationFactory
+            .newPdfTransportation()
+            .serialize(
+                copyPresentationE,
+                new ProtocolProperty(EncodeType.ORIGINAL),
+                weIdAuthentication);
+
+        LogUtil.info(logger, "serialize", response);
+        Assert.assertEquals(ErrorCode.SUCCESS.getCode(), response.getErrorCode().intValue());
+        Assert.assertNotNull(response.getResult());
+    }
+
+    /**
+     * 修改 presentation，设置为两个重复的credentialPojo，序列化成功.
+     */
+    @Test
+    public void testSerialize_credentialPojoDouble() {
+        PresentationE copyPresentationE = DataToolUtils.clone(presentation);
+        List<CredentialPojo> credentialPojoList =  copyPresentationE.getVerifiableCredential();
+        CredentialPojo credentialPojo = credentialPojoList.get(0);
+        CredentialPojo copyCredentialPojo = copyCredentialPojo(credentialPojo);
+        credentialPojoList.add(copyCredentialPojo);
+
+        ResponseData<OutputStream> response = TransportationFactory
+            .newPdfTransportation()
+            .serialize(
+                copyPresentationE,
+                new ProtocolProperty(EncodeType.ORIGINAL),
+                weIdAuthentication);
+
+        LogUtil.info(logger, "serialize", response);
+        Assert.assertEquals(ErrorCode.SUCCESS.getCode(), response.getErrorCode().intValue());
+        Assert.assertNotNull(response.getResult());
+    }
+
+    /**
+     * 修改 presentation，设置为两个不同的credentialPojo，序列化成功.
+     */
+    @Test
+    public void testSerialize_credentialPojoDifferent() {
+        PresentationE copyPresentationE = DataToolUtils.clone(presentation);
+        List<CredentialPojo> credentialPojoList =  copyPresentationE.getVerifiableCredential();
+        credentialPojoList.add(credentialPojoNew);
+
+        ResponseData<OutputStream> response = TransportationFactory
+            .newPdfTransportation()
+            .serialize(
+                copyPresentationE,
+                new ProtocolProperty(EncodeType.ORIGINAL),
+                weIdAuthentication);
+
+        LogUtil.info(logger, "serialize", response);
+        Assert.assertEquals(ErrorCode.SUCCESS.getCode(), response.getErrorCode().intValue());
+        Assert.assertNotNull(response.getResult());
+    }
+
+    /**
+     * 修改 presentation，credentialPojo中的claim为null，序列化成功.
+     */
+    @Test
+    public void testSerialize_credentialPojoClaimNull() {
+        PresentationE copyPresentationE = DataToolUtils.clone(presentation);
+        List<CredentialPojo> credentialPojoList =  copyPresentationE.getVerifiableCredential();
+        CredentialPojo copyCredentialPojo = copyCredentialPojo(credentialPojo);
+        copyCredentialPojo.setClaim(new HashMap<>());
+        credentialPojoList.add(copyCredentialPojo);
+
+        ResponseData<OutputStream> response = TransportationFactory
+            .newPdfTransportation()
+            .serialize(
+                copyPresentationE,
+                new ProtocolProperty(EncodeType.ORIGINAL),
+                weIdAuthentication);
+
+        LogUtil.info(logger, "serialize", response);
+        Assert.assertEquals(ErrorCode.SUCCESS.getCode(), response.getErrorCode().intValue());
+        Assert.assertNotNull(response.getResult());
+    }
+
+    /**
+     * 修改 presentation，修改credentialPojo中的claim的name值，序列化成功.
+     */
+    @Test
+    public void testSerialize_modifyClaimValue() {
+        PresentationE copyPresentationE = DataToolUtils.clone(presentation);
+        List<CredentialPojo> credentialPojoList =  copyPresentationE.getVerifiableCredential();
+        CredentialPojo copyCredentialPojo = copyCredentialPojo(credentialPojo);
+        Map<String, Object> claim = copyCredentialPojo.getClaim();
+        claim.replace("name", "rockyxia");
+        credentialPojoList.add(copyCredentialPojo);
+
+        ResponseData<OutputStream> response = TransportationFactory
+            .newPdfTransportation()
+            .serialize(
+                copyPresentationE,
+                new ProtocolProperty(EncodeType.ORIGINAL),
+                weIdAuthentication);
+
+        LogUtil.info(logger, "serialize", response);
+        Assert.assertEquals(ErrorCode.SUCCESS.getCode(), response.getErrorCode().intValue());
+        Assert.assertNotNull(response.getResult());
+    }
+
+    /**
+     * 修改 presentation，增加credentialPojo中的claim新的键值对，序列化成功.
+     */
+    @Test
+    public void testSerialize_addClaimValue() {
+        PresentationE copyPresentationE = DataToolUtils.clone(presentation);
+        List<CredentialPojo> credentialPojoList =  copyPresentationE.getVerifiableCredential();
+        CredentialPojo copyCredentialPojo = copyCredentialPojo(credentialPojo);
+        Map<String, Object> claim = copyCredentialPojo.getClaim();
+        claim.put("company", "webank");
+        credentialPojoList.add(copyCredentialPojo);
+
+        ResponseData<OutputStream> response = TransportationFactory
+            .newPdfTransportation()
+            .serialize(
+                copyPresentationE,
+                new ProtocolProperty(EncodeType.ORIGINAL),
+                weIdAuthentication);
+
+        LogUtil.info(logger, "serialize", response);
+        Assert.assertEquals(ErrorCode.SUCCESS.getCode(), response.getErrorCode().intValue());
+        Assert.assertNotNull(response.getResult());
+    }
+
+    /**
+     * 修改 presentation，删除credentialPojo中的claim新的键值对，序列化成功.
+     */
+    @Test
+    public void testSerialize_delClaimValue() {
+        PresentationE copyPresentationE = DataToolUtils.clone(presentation);
+        List<CredentialPojo> credentialPojoList =  copyPresentationE.getVerifiableCredential();
+        CredentialPojo copyCredentialPojo = copyCredentialPojo(credentialPojo);
+        Map<String, Object> claim = copyCredentialPojo.getClaim();
+        claim.remove("name");
+        credentialPojoList.add(copyCredentialPojo);
+
+        ResponseData<OutputStream> response = TransportationFactory
+            .newPdfTransportation()
+            .serialize(
+                copyPresentationE,
+                new ProtocolProperty(EncodeType.ORIGINAL),
+                weIdAuthentication);
+
+        LogUtil.info(logger, "serialize", response);
+        Assert.assertEquals(ErrorCode.SUCCESS.getCode(), response.getErrorCode().intValue());
+        Assert.assertNotNull(response.getResult());
+    }
+
+    /**
+     * 修改 presentation.content值，序列化成功.
+     */
+    @Test
+    public void testSerialize_modifyContent() {
+        PresentationE copyPresentationE = DataToolUtils.clone(presentation);
+        List<String> content = new ArrayList<>();
+        copyPresentationE.setContext(content);
+
+        ResponseData<OutputStream> response = TransportationFactory
+            .newPdfTransportation()
+            .serialize(
+                copyPresentationE,
+                new ProtocolProperty(EncodeType.ORIGINAL),
+                weIdAuthentication);
+
+        LogUtil.info(logger, "serialize", response);
+        Assert.assertEquals(ErrorCode.SUCCESS.getCode(), response.getErrorCode().intValue());
+        Assert.assertNotNull(response.getResult());
+    }
+
+    /**
+     * 修改 presentation.proof为null值，序列化成功.
+     */
+    @Test
+    public void testSerialize_setProoofNull() {
+        PresentationE copyPresentationE = DataToolUtils.clone(presentation);
+        Map<String, Object> proof = new HashMap<>();
+        copyPresentationE.setProof(proof);
+
+        ResponseData<OutputStream> response = TransportationFactory
+            .newPdfTransportation()
+            .serialize(
+                copyPresentationE,
+                new ProtocolProperty(EncodeType.ORIGINAL),
+                weIdAuthentication);
+
+        LogUtil.info(logger, "serialize", response);
+        Assert.assertEquals(ErrorCode.SUCCESS.getCode(), response.getErrorCode().intValue());
+        Assert.assertNotNull(response.getResult());
+    }
+
+    /**
+     * 修改 presentation.credentialPojo.proof为null值，序列化成功.
+     */
+    @Test
+    public void testSerialize_setCredentialPojoProoofNull() {
+        PresentationE copyPresentationE = DataToolUtils.clone(presentation);
+        List<CredentialPojo> credentialPojoList =  copyPresentationE.getVerifiableCredential();
+        CredentialPojo copyCredentialPojo = copyCredentialPojo(credentialPojo);
+        copyCredentialPojo.setProof(new HashMap<>());
+        credentialPojoList.add(copyCredentialPojo);
+
+        ResponseData<OutputStream> response = TransportationFactory
+            .newPdfTransportation()
+            .serialize(
+                copyPresentationE,
+                new ProtocolProperty(EncodeType.ORIGINAL),
+                weIdAuthentication);
+
+        LogUtil.info(logger, "serialize", response);
+        Assert.assertEquals(ErrorCode.SUCCESS.getCode(), response.getErrorCode().intValue());
+        Assert.assertNotNull(response.getResult());
+    }
+
+    /**
+     * 序列化credentialPojo，序列化成功.
+     */
+    @Test
+    public void testSerialize_credentialPojo() {
+        ResponseData<OutputStream> response = TransportationFactory
+            .newPdfTransportation()
+            .serialize(
+                credentialPojoNew,
+                new ProtocolProperty(EncodeType.ORIGINAL),
+                weIdAuthentication);
+
+        LogUtil.info(logger, "serialize", response);
+        Assert.assertEquals(ErrorCode.SUCCESS.getCode(), response.getErrorCode().intValue());
+        Assert.assertNotNull(response.getResult());
+    }
+
+    /**
+     * 序列化selectiveCredentialPojo，序列化成功.
+     */
+    @Test
+    public void testSerialize_selectiveCredentialPojo() {
+        ResponseData<OutputStream> response = TransportationFactory
+            .newPdfTransportation()
+            .serialize(
+                selectiveCredentialPojo,
+                new ProtocolProperty(EncodeType.ORIGINAL),
+                weIdAuthentication);
+
+        LogUtil.info(logger, "serialize", response);
+        Assert.assertEquals(ErrorCode.SUCCESS.getCode(), response.getErrorCode().intValue());
+        Assert.assertNotNull(response.getResult());
+    }
+
+    /**
+     * 序列化selectiveCredentialPojo，序列化成功.
+     */
+    @Test
+    public void testSerialize_credential() {
+
+        List<CredentialPojo> credPojoList = new ArrayList<>();
+        credPojoList.add(selectiveCredentialPojo);
+        CredentialPojo doubleSigned =
+            credentialPojoService.addSignature(credPojoList, weIdAuthentication)
+            .getResult();
+        ResponseData<OutputStream> response = TransportationFactory
+            .newPdfTransportation()
+            .serialize(
+                doubleSigned,
+                new ProtocolProperty(EncodeType.ORIGINAL),
+                weIdAuthentication);
+
+        LogUtil.info(logger, "serialize", response);
+        Assert.assertEquals(ErrorCode.SUCCESS.getCode(), response.getErrorCode().intValue());
+        Assert.assertNotNull(response.getResult());
+    }
 
     /**
      * 多CPT测试.
@@ -199,12 +535,14 @@ public class TestPdfSerialize extends TestBaseTransportation {
                 weIdAuthentication,
                 "src/test/resources/test-template.pdf"
                 );
+        System.out.println(presentation4SpecTpl.toJson());
         LogUtil.info(logger, "serialize", response);
         Assert.assertEquals(
             ErrorCode.SUCCESS.getCode(),
             response.getErrorCode().intValue());
         Assert.assertNotNull(response.getResult());
     }
+
 
     /**
      * 传入指定模板目录为空字符串.
